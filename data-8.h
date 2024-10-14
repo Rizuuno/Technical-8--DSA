9769e5f2432eda8e65a1cabb76cee8d27e6a56cf
Vince John Parocha <vincejohn.parocha@gmail.com>
2024-10-15 01:36:52 +0800

    Add files via upload

---
+++ data-8.h
@@ -0,0 +1,16 @@
+const int MAX_VERTICES = 100;
+
+class Graph {
+    private:
+        int num_vertices;
+        int adj_matrix[MAX_VERTICES][MAX_VERTICES];
+
+    public:
+        Graph(int vertices);
+		void add_edge(int u, int v);
+        void remove_edge(int u, int v);
+        void bfs(int start_vertex);
+        void dfs_traversal(int start_vertex);
+        void dfs(int vertex, bool visited[]);
+        void display_matrix();
+};
